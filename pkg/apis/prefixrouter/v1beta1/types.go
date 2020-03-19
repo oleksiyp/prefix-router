748b274b986b9ca6544dfe9afed5a75ffa13d523
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2020-03-19 20:29:19 +0100

    Route

---
+++ pkg/apis/prefixrouter/v1beta1/types.go
@@ -0,0 +1,37 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Route is a specification for a Route resource
+type Route struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RouteSpec   `json:"spec"`
+	Status RouteStatus `json:"status"`
+}
+
+// RouteSpec is the spec for a Route resource
+type RouteSpec struct {
+	Prefix  string `json:"deploymentName"`
+	Service string `json:"replicas"`
+}
+
+// RouteStatus is the status for a Route resource
+type RouteStatus struct {
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RouteList is a list of Route resources
+type RouteList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Route `json:"items"`
+}
